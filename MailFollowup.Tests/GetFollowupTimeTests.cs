875de1d74cf855bc092d5e9fe8cf90e9e2b65329
Robin Meischke <robin@nierhain.de>
2024-04-02 12:44:41 +0200

    feat: add basic parsing

--- MailFollowup.Tests/GetFollowupTimeTests.cs
+++ MailFollowup.Tests/GetFollowupTimeTests.cs
@@ -1,11 +1,65 @@
+using FluentAssertions;
+using MailFollowup.Tests.TheoryGenerators;
+
 namespace MailFollowup.Tests;
-// Given_When_Then
+// Given_Then
 // (A)rrange (A)ct (A)ssert  
 public class GetFollowupTimeTests
 {
+    private readonly Main _sut;
+    public GetFollowupTimeTests()
+    {
+        _sut = new Main();
+    }
 
     [Fact]
-    public void Given_When_Then()
+    public void GivenAValidDateTime_ReturnAValidDateTimeAfter()
     {
+        //Arrange
+        var date = new DateTime();
+        var followup = "1hour";
+        //Act
+        var t = _sut.GetFollowupTime(date, followup);
+        //Assert
+        t.Should().BeAfter(date);
+    }
+
+    [Fact]
+    public void Given2Weeks_ReturnDateTimeIn2Weeks()
+    {
+        //Arrange
+        var date = new DateTime(2024, 3, 4);
+        var followup = "2weeks";
+        var expected = new DateTime(2024, 3, 18);
+        //Act
+        var t = _sut.GetFollowupTime(date, followup);
+        //Assert
+        t.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Given1Week4Hours_ReturnDateTimeIn1WeekAnd4Hours()
+    {
+        //Arrange
+        var date = new DateTime(2024, 4,2, 12, 0, 0);
+        var followup = "1week4hours";
+        var expected = new DateTime(2024, 4, 9, 16, 0, 0);
+        //Act
+        var t = _sut.GetFollowupTime(date, followup);
+        //Assert
+        t.Should().Be(expected);
+    }
+
+    [Fact]
+    public void GivenAFaultyFollowup_ThrowArgumentException()
+    {
+        //Arrange
+        var date = new DateTime(2024, 4, 2, 12, 0, 0);
+        var followup = "undefined";
+        //Act
+        Action act = () => _sut.GetFollowupTime(date, followup);
+        //Assert
+        act.Should().Throw<ArgumentException>()
+            .WithMessage($"Followup address '{followup}' is invalid.");
     }
 }
--- MailFollowup/Main.cs
+++ MailFollowup/Main.cs
@@ -1,5 +1,39 @@
-﻿namespace MailFollowup;
+﻿using System.Text.RegularExpressions;
+
+namespace MailFollowup;
 
 public class Main
 {
+    public DateTime GetFollowupTime(DateTime now, string address)
+    {
+        if (!IsAddressValid(address))
+        {
+            throw new ArgumentException($"Followup address '{address}' is invalid.");
+        }
+        
+        var days = GetDays(address);
+        var hours = GetHours(address);
+        
+        return now.AddDays(days).AddHours(hours);
+    }
+
+    private bool IsAddressValid(string address)
+    {
+        return address.Contains("hour") || address.Contains("week");
+    }
+
+    private int GetDays(string address)
+    {
+        if (!address.Contains("week")) return 0;
+        var weekString = address.Split("week")[0];
+        return Convert.ToInt32(weekString) * 7;
+    }
+
+    private int GetHours(string address)
+    {
+        if (!address.Contains("hour")) return 0;
+        var hourString = address.Split("hour")[0];
+        var lastIndex = hourString.LastIndexOfAny(['s', 'k', 'r']);
+        return Convert.ToInt32(hourString.Substring(lastIndex + 1));
+    }
 }
